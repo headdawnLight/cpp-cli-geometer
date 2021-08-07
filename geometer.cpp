a648474059bbf6e8702aad9343881804896cf59b
Sertan Işık <sertanix@yandex.com>
2021-08-07 21:59:26 +0300

    Update geometer.cpp

--- geometer.cpp
+++ geometer.cpp
@@ -1,9 +1,80 @@
 // Geometer programming with C++ on MS Visual Studio 2019
 // geometer.cpp
-#include <iostream>
-using namespace std;
 
-void DrawRectangle()
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+void showMenu();
+void runMenu();
+void showSquare();
+void calculateSquare();
+void showTriangle();
+void calculateTriangle();
+void showRectangle();
+void calculateRectangle();
+
+char main()
+{   
+    string Menu = "Welcome to GEOMETER!";
+    cout << Menu << endl;
+    runMenu();
+    return 0;
+}
+
+void showMenu() {
+    cout << "\nPress \"1\" for SQUARE." << endl;
+    cout << "Press \"2\" for TRIANGLE." << endl;
+    cout << "Press \"3\" for RECTANGLE." << endl;
+}
+
+void runMenu() 
+{
+        showMenu();
+
+        short choice;
+
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            calculateSquare();
+        }
+        else if (choice == 2) {
+            calculateTriangle();
+        }     
+        else if (choice == 3) {
+            calculateRectangle();
+        }   
+        else {
+            runMenu();
+        }        
+}
+
+void showSquare()
+{
+    cout << "--------------------" << endl;
+}
+
+void calculateSquare()
+{
+    cout << "--------------------" << endl;
+    runMenu();
+}
+
+void showTriangle()
+{
+    cout << "--------------------" << endl;
+}
+
+void calculateTriangle()
+{
+    cout << "--------------------" << endl;
+    runMenu();
+}
+
+void showRectangle()
 {
     cout << "--------------------" << endl;
     cout << "|                  |" << endl;
@@ -13,25 +84,27 @@ void DrawRectangle()
     cout << "    A(Long edge)    " << endl;
     cout << endl << "Perimeter of rectangle: (2*A)+(2*B)" << endl;
     cout << endl << "Area of rectangle: A*B" << endl;
-    cout << endl << "Measurement unit: Metric" << endl;
+    cout << endl << "Measurement unit: Meter" << endl;
 }
-int main()
+
+void calculateRectangle() 
 {
-    DrawRectangle();
+    showRectangle();
 
-	double rectShort, rectLong, rectPeri, rectArea;
-	
-		cout << "\nEnter the short edge of the rectangle: ";
-		cin >> rectShort;
-		cout << "\nEnter the long edge of the rectangle: ";
-		cin >> rectLong;
+    int rectShort, rectLong, rectPeri, rectArea;
 
-        rectPeri = (2 * rectShort) + (2 * rectLong);
+    cout << "\nEnter the short edge of the rectangle: ";
+    cin >> rectShort;
 
-        rectArea = rectShort * rectLong;
+    cout << "\nEnter the long edge of the rectangle: ";
+    cin >> rectLong;
 
-        cout << "\nPerimeter of the rectangle:" << rectPeri << endl;
-        cout << "\nArea of the rectangle:" << rectArea << endl;
+    rectPeri = (2 * rectShort) + (2 * rectLong);
 
-        return 0;
+    rectArea = rectShort * rectLong;
+
+    cout << "\nPerimeter of the rectangle:" << rectPeri << endl;
+    cout << "\nArea of the rectangle:" << rectArea << endl;
+
+    runMenu();
 }
