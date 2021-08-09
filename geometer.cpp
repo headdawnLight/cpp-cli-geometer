02ab72b7c2f056ba438000eab1020d5ef20f4f8b
Sertan Işık <sertanix@yandex.com>
2021-08-09 23:45:38 +0300

    Uptade geometer.cpp

--- geometer.cpp
+++ geometer.cpp
@@ -15,151 +15,162 @@ void calculateTriangle();
 void showRectangle();
 void calculateRectangle();
 
-char main()
+char main() // Works with nested global void functions.
 {   
-    string Menu = "Welcome to GEOMETER!";
+    string Menu = "---Welcome to GEOMETER!";
     cout << Menu << endl;
     runMenu();
     return 0;
 }
 
-void showMenu() {
-    cout << "\nPress \"1\" for SQUARE." << endl;
-    cout << "Press \"2\" for TRIANGLE." << endl;
-    cout << "Press \"3\" for RECTANGLE." << endl;
+void showMenu() // Two-dimensional geometric shapes via command line interface.
+{
+    cout << "\n-->Press \"1\" for SQUARE." << endl;
+    cout << "-->Press \"2\" for TRIANGLE." << endl;
+    cout << "-->Press \"3\" for RECTANGLE." << endl;
 }
 
-void runMenu() 
+void runMenu() // Runs the choice and shows the menu every time.
 {
-        showMenu();
+    showMenu();
 
-        short choice;
+    int choice;
 
-        cout << "\nEnter your choice: ";
+        cout << "\n-->Enter your choice: ";
         cin >> choice;
+        cout << "\n" << endl;
 
-        if (choice == 1) {
+        if (choice < 1 || choice > 3)
+        {
+            cout << "---An invalid choice! Choose again!" << endl << endl;
+            runMenu();
+        }
+       
+        if (choice == 1)
+        {
             calculateSquare();
         }
-        else if (choice == 2) {
+        else if (choice == 2)
+        {
             calculateTriangle();
-        }     
-        else if (choice == 3) {
+        }
+        else if (choice == 3)
+        {
             calculateRectangle();
-        }   
+        }
         else {
             runMenu();
-        }        
+        }
 }
 
-void showSquare()
+void showSquare() // Shows two-dimensional square and its geometric formulas.
 {
-    cout << "      A    " << endl;
-    cout << "  ---------" << endl;
-    cout << "  |       |" << endl;
-    cout << "A |       | A" << endl;
-    cout << "  |       |" << endl;
-    cout << "  ---------" << endl;
-    cout << "      A (Edge of square)    " << endl;
-    cout << endl << "Perimeter of square: 4*A" << endl;
-    cout << endl << "Area of square: A*A" << endl;
-    cout << endl << "Measurement unit: Meter" << endl;
+    cout << "         A    " << endl;
+    cout << "     ---------" << endl;
+    cout << "     |       |" << endl;
+    cout << "   A |       | A" << endl;
+    cout << "     |       |" << endl;
+    cout << "     ---------" << endl;
+    cout << "         A (Edge of square)    " << endl << endl;
+    cout << "   Perimeter of square: 4*A" << endl << endl;
+    cout << "   Area of square: A*A" << endl;
 }
 
-void calculateSquare()
+void calculateSquare()  // Calculates the perimeter and area of square.
 {
     showSquare();
 
     double squareEdge, squarePeri, squareArea;
 
-    cout << "\nEnter the edge of the square: ";
+    cout << "\n-->Enter the edge of the square: ";
     cin >> squareEdge;
 
     squarePeri = 4 * squareEdge;
 
-    squareArea = squareEdge * squareEdge;
+    squareArea = squareEdge * squareEdge; 
 
-    cout << "\nPerimeter of the square: " << squarePeri << endl;
-    cout << "\nArea of the square: " << squareArea << endl;
-
+    cout << "\n---Perimeter of the square: " << squarePeri << " meters or etc." << endl;
+    cout << "\n---Area of the square: " << squareArea << " square meters or etc." 
+         << endl << endl << endl;
+        
     runMenu();
 }
 
-void showTriangle()
+void showTriangle() // Shows two-dimensional triangle and its geometric formulas.
 {
-    cout << "     /\\   " << endl;
-    cout << "    /| \\      \"A\" is the edge of the triangle." << endl;
-    cout << "   / |  \\     \"B\" is the base of the triangle." << endl;
-    cout << "A /  |h  \\ C  \"C\" is the other edge of the triangle." << endl;
-    cout << " /   |    \\   \"h\" is the height of the triangle." << endl;
-    cout << "/    |     \\" << endl;
-    cout << "-------------" << endl;
-    cout << "      B      " << endl;
-    cout << endl << "Perimeter of triangle: A+B+C" << endl;
-    cout << endl << "Area of triangle: (B*h)/2" << endl;
-    cout << endl << "Measurement unit: Meter" << endl;
+    cout << "        /\\   " << endl;
+    cout << "       /| \\      \"A\" is the edge of the triangle." << endl;
+    cout << "      / |  \\     \"B\" is the base of the triangle." << endl;
+    cout << "   A /  |h  \\ C  \"C\" is the other edge of the triangle." << endl;
+    cout << "    /   |    \\   \"h\" is the height of the triangle." << endl;
+    cout << "   /    |     \\" << endl;
+    cout << "   -------------" << endl;
+    cout << "         B      " << endl << endl;
+    cout << "   Perimeter of triangle: A+B+C" << endl << endl;
+    cout << "   Area of triangle: (B*h)/2" << endl;
 }
 
-void calculateTriangle()
+void calculateTriangle() // Calculates the perimeter and area of triangle.
 {
     showTriangle();
 
     double triA, triB, triC, triH, triPeri, triArea;
 
-    cout << "\nEnter the edge of the triangle: ";
+    cout << "\n-->Enter the edge of the triangle: ";
     cin >> triA;
 
-    cout << "\nEnter the base of the triangle: ";
+    cout << "\n-->Enter the base of the triangle: ";
     cin >> triB;
 
-    cout << "\nEnter the other edge of the triangle: ";
+    cout << "\n-->Enter the other edge of the triangle: ";
     cin >> triC;
 
-    cout << "\nEnter the height of the triangle: ";
+    cout << "\n-->Enter the height of the triangle: ";
     cin >> triH;
 
     triPeri = triA + triB + triC;
 
     triArea = ((triB*triH) / 2);
 
-    cout << "\nPerimeter of the triangle: " << triPeri << endl;
-    cout << "\nArea of the triangle: " << triArea << endl;
+    cout << "\n---Perimeter of the triangle: " << triPeri << " meters or etc." << endl;
+    cout << "\n---Area of the triangle: " << triArea << " square meters or etc."
+         << endl << endl << endl;
 
     runMenu();
 }
 
-void showRectangle()
+void showRectangle() // Shows two-dimensional rectangle and its geometric formulas.
 {
-    cout << "           A          " << endl;
-    cout << "  --------------------" << endl;
-    cout << "  |                  |" << endl;
-    cout << "B |                  | B (Short edge of rectangle)" << endl;
-    cout << "  |                  |" << endl;
-    cout << "  --------------------" << endl;
-    cout << "           A (Long edge of rectangle)    " << endl;
-    cout << endl << "Perimeter of rectangle: (2*A)+(2*B)" << endl;
-    cout << endl << "Area of rectangle: A*B" << endl;
-    cout << endl << "Measurement unit: Meter" << endl;
+    cout << "              A          " << endl;
+    cout << "     --------------------" << endl;
+    cout << "     |                  |" << endl;
+    cout << "   B |                  | B (Short edge of rectangle)" << endl;
+    cout << "     |                  |" << endl;
+    cout << "     --------------------" << endl;
+    cout << "              A (Long edge of rectangle)" << endl << endl;
+    cout << "   Perimeter of rectangle: (2*A)+(2*B)" << endl << endl;
+    cout << "   Area of rectangle: A*B" << endl;
 }
 
-void calculateRectangle() 
+void calculateRectangle() // Calculates the perimeter and area of rectangle.
 {
     showRectangle();
 
     double rectShort, rectLong, rectPeri, rectArea;
 
-    cout << "\nEnter the short edge of the rectangle: ";
+    cout << "\n-->Enter the short edge of the rectangle: ";
     cin >> rectShort;
 
-    cout << "\nEnter the long edge of the rectangle: ";
+    cout << "\n-->Enter the long edge of the rectangle: ";
     cin >> rectLong;
 
     rectPeri = (2 * rectShort) + (2 * rectLong);
 
     rectArea = rectShort * rectLong;
 
-    cout << "\nPerimeter of the rectangle: " << rectPeri << endl;
-    cout << "\nArea of the rectangle: " << rectArea << endl;
+    cout << "\n---Perimeter of the rectangle: " << rectPeri << " meters or etc." << endl;
+    cout << "\n---Area of the rectangle: " << rectArea << " square meters or etc."
+         << endl << endl << endl;
 
     runMenu();
 }
